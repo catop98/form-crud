a3c646a818172c0ff9f827bea6b47791d912b199
camilotorres <camivad@gmail.com>
2022-06-02 15:55:57 -0500

    my first commit

---
+++ src/components/Cabecera_head_main.vue
@@ -0,0 +1,35 @@
+<template>
+<div>
+    <div class="header">
+		<ul>
+			<li class="active form_1_progessbar">
+				<div>
+					<p>1</p>
+				</div>
+			</li>
+			<li class="form_2_progessbar">
+				<div>
+					<p>2</p>
+				</div>
+			</li>
+			<li class="form_3_progessbar">
+				<div>
+					<p>3</p>
+				</div>
+			</li>
+			<li class="form_4_progessbar">
+				<div>
+					<p>4</p>
+				</div>
+			</li>
+		</ul>
+	</div>
+</div>
+    
+</template>
+
+<script>
+export default{
+    name:'Cabecera_head_main'
+}
+</script>
---
+++ src/components/Form_one.vue
@@ -0,0 +1,36 @@
+<template>
+<div class="form_wrap">
+    <div class="form_1 data_info">
+        <h2>¿Qué tan probable es que recomiende eventos de eventMB a un amigo o colega?</h2>
+		<!-- eslint-disable no-mixed-spaces-and-tabs -->
+            <div class="button-wrapper">
+				<button class="efect">0</button>
+				<button class="efect">1</button>
+				<button class="efect">2</button>
+				<button class="efect">3</button>
+				<button class="efect">4</button>
+				<button class="efect">5</button>
+				<button class="efect">6</button>
+				<button class="efect">7</button>
+				<button class="efect">8</button>
+				<button class="efect">9</button>
+				<button class="efect">10</button>
+		    </div>
+        
+    </div>
+
+	<!-- eslint-disable no-mixed-spaces-and-tabs -->
+
+</div>
+
+
+
+
+</template>
+
+<script>
+export default{
+    name:'Form_one'
+}
+</script>
+
